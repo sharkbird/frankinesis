40c1663e21bb1ef2a52b71959b8403d364ea67de
sharkbird <jason@sharkbird.com>
2023-04-20 14:53:43 -0500

    changed thumb ctl keys  to be GUI keys to work  well with i3w window manager.  moved control keys to bottom row.

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -13,20 +13,20 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_DVORAK] = LAYOUT_kinesis(
-  KC_ESC,         KC_1,           KC_2,           KC_3,           KC_4,          KC_5,           KC_6,           KC_7,           KC_8,          KC_9,           KC_0,            KC_SLSH,
-  KC_TAB,         KC_QUOT,        KC_COMM,        KC_DOT,         KC_P,          KC_Y,           KC_F,           KC_G,           KC_C,          KC_R,           KC_L,            KC_BSLS,
-  KC_GRV,         KC_A,           KC_O,           KC_E,           KC_U,          KC_I,           KC_D,           KC_H,           KC_T,          KC_N,           KC_S,            KC_MINS,
-  KC_LSFT,        KC_SCLN,        KC_Q,           KC_J,           KC_K,          KC_X,           KC_B,           KC_M,           KC_W,          KC_V,           KC_Z,            KC_RSFT,
-  KC_NO,          KC_LALT,        KC_LGUI,        KC_LEFT,        KC_RIGHT,      KC_SPC,         KC_BSPC,        KC_UP,          KC_DOWN,       KC_LBRC,        KC_EQL,          KC_NO,
-  KC_LALT,        KC_HOME,        KC_END,         KC_LCTL,        KC_DEL,        KC_BSPC,        KC_SPC,         KC_ENTER,       KC_RCTL,       KC_PGDN,        KC_PGUP,         KC_RALT,
-  KC_NO,          KC_DEL,         KC_PGUP,        KC_PGDN,        KC_DEL,        KC_ENTER,       KC_UP,          KC_DOWN,        KC_ENTER,      KC_SPC,         KC_NO,           KC_NO,
-  KC_NO,          KC_DEL,         KC_PGUP,        KC_PGDN,        KC_DEL,        KC_ENTER,       KC_UP,          KC_DOWN,        KC_ENTER,      KC_SPC,         KC_NO,           KC_NO
+  KC_ESC,         KC_1,           KC_2,           KC_3,           KC_4,                 KC_5,                   KC_6,           KC_7,           KC_8,          KC_9,           KC_0,            KC_SLSH,
+  KC_TAB,         KC_QUOT,        KC_COMM,        KC_DOT,         KC_P,                 KC_Y,                   KC_F,           KC_G,           KC_C,          KC_R,           KC_L,            KC_BSLS,
+  KC_GRV,         KC_A,           KC_O,           KC_E,           KC_U,                 KC_I,                   KC_D,           KC_H,           KC_T,          KC_N,           KC_S,            KC_MINS,
+  KC_LSFT,        KC_SCLN,        KC_Q,           KC_J,           KC_K,                 KC_X,                   KC_B,           KC_M,           KC_W,          KC_V,           KC_Z,            KC_RSFT,
+  KC_NO,          KC_LCTL,        KC_LGUI,        KC_LEFT,        KC_RIGHT,             KC_RIGHT,               KC_UP,          KC_UP,          KC_DOWN,       KC_LBRC,        KC_RCTL,          KC_NO,
+  KC_LALT,        KC_HOME,        KC_END,         KC_LGUI,        KC_DEL, LT(_SYMNUM,  KC_BSPC),               LT(_SYMNUM, KC_SPACE), KC_ENTER, KC_LGUI,      KC_PGDN,        KC_PGUP,         KC_RALT,
+  KC_NO,          KC_NO,         KC_NO,        KC_NO,        KC_NO,               KC_NO,               KC_NO,          KC_NO,        KC_NO,      KC_NO,         KC_NO,           KC_NO,
+  KC_NO,          KC_NO,         KC_NO,        KC_NO,        KC_NO,               KC_NO,               KC_NO,          KC_NO,        KC_NO,      KC_NO,         KC_NO,           KC_NO
  ),
 
  [_SYMNUM] = LAYOUT_kinesis(
   KC_F11,         KC_F1,          KC_F2,          KC_F3,          KC_F4,         KC_F5,          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F12,
-  KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_AMPR,       KC_TRNS,        KC_PERC,        KC_EQL,         KC_LT,          KC_GT,          KC_EXLM,        KC_TRNS,
-  KC_TRNS,        KC_LCBR,        KC_LBRC,        KC_LPRN,        KC_0,          KC_MINS,        KC_PLUS,        KC_1,           KC_RPRN,        KC_RBRC,        KC_RCBR,        KC_TRNS,
+  QMKSO,          KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_AMPR,       KC_TRNS,        KC_PERC,        KC_EQL,         KC_LT,          KC_GT,          KC_EXLM,        QMKPC,
+  KC_TRNS,        KC_LCBR,        KC_LBRC,        KC_LPRN,        KC_0,          KC_MINS,        KC_PLUS,        KC_1,           KC_RPRN,        KC_RBRC,        KC_RCBR,        QMKPD,
   CW_TOGG,        KC_6,           KC_7,           KC_8,           KC_9,          KC_SLSH,        KC_ASTR,        KC_2,           KC_3,           KC_4,           KC_5,           KC_TRNS,
   KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,       KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
   KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,       KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,
